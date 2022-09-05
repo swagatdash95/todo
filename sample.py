dd12b07bc2ce6592737208f325303c1f20ee6f56
Swagat Dash <swagatdash@Swagats-MacBook-Air.local>
2022-09-06 00:23:05 +0100

    Setup commit

---
+++ sample.py
@@ -0,0 +1,13 @@
+def custom_sum(n1: int, n2: int) -> int:
+    '''Sum Function'''
+    return n1 + n2
+
+
+def sub(n1, n2):
+    return n1-n2
+
+
+result = custom_sum(1, 2.5)
+print(result)
+result = sub(1, 2.5)
+print(result)
